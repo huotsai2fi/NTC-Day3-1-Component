d346daf975d8fa8f0dfa3c899be0d9d32e7e545f
April Hung <huotsai2fi@gmail.com>
2022-02-19 23:36:45 +0800

    perf: move v-if values from methods to computed

--- 1_button_outside_component/app.js
+++ 1_button_outside_component/app.js
@@ -10,6 +10,15 @@ const app = Vue.createApp({
       comps: ['first', 'second', 'third'],
     };
   },
+  computed: {
+    noUpPage() {
+      return this.comps.indexOf(this.currentComp) === 0;
+    },
+    noDownPage() {
+      console.log(this.comps.indexOf(this.currentComp));
+      return this.comps.indexOf(this.currentComp) === this.comps.length - 1;
+    },
+  },
   methods: {
     prev() {
       const index = this.comps.indexOf(this.currentComp);
@@ -18,13 +27,6 @@ const app = Vue.createApp({
     next() {
       const index = this.comps.indexOf(this.currentComp);
       this.currentComp = this.comps[index + 1];
-    },
-    noUpPage() {
-      return this.comps.indexOf(this.currentComp) === 0;
-    },
-    noDownPage() {
-      console.log(this.comps.indexOf(this.currentComp));
-      return this.comps.indexOf(this.currentComp) === this.comps.length - 1;
     },
   },
 });
