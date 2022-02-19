75d812ab9b0a1bbc39106940eaffef42630e86db
April Hung <huotsai2fi@gmail.com>
2022-02-19 15:15:48 +0800

    add files

---
+++ app.js
@@ -0,0 +1,47 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      user: {
+        username: '',
+        phone: '',
+        address: '',
+      },
+      currentComp: 'first',
+      comps: ['first', 'second', 'third'],
+    };
+  },
+  methods: {
+    prev() {
+      const index = this.comps.indexOf(this.currentComp);
+      this.currentComp = this.comps[index - 1];
+    },
+    next() {
+      const index = this.comps.indexOf(this.currentComp);
+      this.currentComp = this.comps[index + 1];
+    },
+    noUpPage() {
+      return this.comps.indexOf(this.currentComp) === 0;
+    },
+    noDownPage() {
+      console.log(this.comps.indexOf(this.currentComp));
+      return this.comps.indexOf(this.currentComp) === this.comps.length - 1;
+    },
+  },
+});
+
+app.component('first', {
+  template: '#first',
+  props: ['user-info'],
+});
+
+app.component('second', {
+  template: '#second',
+  props: ['user-info'],
+});
+
+app.component('third', {
+  template: '#third',
+  props: ['user-info'],
+});
+
+app.mount('#app');
